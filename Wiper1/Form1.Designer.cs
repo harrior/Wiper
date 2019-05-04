3132ea26116d28450a017fa49cd00a71f637f62b
harrior <harrior@users.noreply.github.com>
2019-05-04 21:44:24 +0300

    Copy old project to master

---
+++ Wiper1/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace Wiper1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(6, 18);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(105, 17);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Random - 1-pass";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(6, 41);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(135, 17);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "DoD 5220.22-M 3-pass";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(6, 64);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(101, 17);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Shnaider 7-pass";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButton4);
+            this.groupBox1.Controls.Add(this.radioButton3);
+            this.groupBox1.Controls.Add(this.radioButton1);
+            this.groupBox1.Controls.Add(this.radioButton2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(167, 132);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Erase method";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(104, 150);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Apply";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(6, 87);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(108, 17);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Gutmann 35-pass";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(190, 185);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Settings";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RadioButton radioButton4;
+    }
+}
---
+++ Wiper1/Program.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+
+namespace Wiper1
+{
+    class Program
+    {
+
+        static int ReWriteByte(string filepath, byte val)
+        {
+            System.IO.FileInfo fileinfo = new System.IO.FileInfo(filepath);
+            long filelength = fileinfo.Length;
+
+            System.IO.BinaryWriter writer = new System.IO.BinaryWriter(System.IO.File.Open(filepath, System.IO.FileMode.OpenOrCreate));
+
+            for (long i = 0; i < filelength; i++)
+            {
+                writer.Write(val);
+            }
+
+            writer.Close();
+            return 0;
+        }
+
+        //Перезапись файла случайными данными
+        static int ReWriteRand(string filepath)
+        {
+            System.IO.FileInfo fileinfo = new System.IO.FileInfo(filepath);
+            long filelength = fileinfo.Length;
+
+            System.IO.BinaryWriter writer = new System.IO.BinaryWriter(System.IO.File.Open(filepath, System.IO.FileMode.OpenOrCreate));
+            Random rand = new Random();
+            byte[] val = new byte[1];
+
+            for (long i = 0; i < filelength; i++)
+            {
+                rand.NextBytes(val);
+                writer.Write(val);
+            }
+
+            writer.Close();
+            return 0;
+        }
+
+        //обнуление файла
+        static void ReWriteEmpty(string filepath)
+        {
+            System.IO.FileInfo fileinfo = new System.IO.FileInfo(filepath);
+            System.IO.BinaryWriter writer = new System.IO.BinaryWriter(System.IO.File.Open(filepath, System.IO.FileMode.Truncate));
+            writer.Close();
+        }
+
+        //Генератор случайных имен файлов
+        static string GenRandomString(int Length)
+        {
+            string Alphabet = "QWERTYUIOPASDFGHJKLZXCVBNMqwertyuiopasdfghjklzxcvbnm1234567890";
+            Random rnd = new Random();
+            StringBuilder sb = new StringBuilder(Length - 1);
+            int Position = 0;
+
+            for (int i = 0; i < Length; i++)
+            {
+                Position = rnd.Next(0, Alphabet.Length - 1);
+                sb.Append(Alphabet[Position]);
+            }
+            return sb.ToString();
+        }
+
+        //Основная функция, перезвписывает файл
+        static void WipeFile(string filename, byte method)
+        {
+            //method 0 - один проход random
+            if (method == 0)
+            {
+                ReWriteRand(filename);
+            }
+            // 1 - DoD 5220.22-M - 3 прохода
+            else if (method == 1)
+            {
+                ReWriteByte(filename, 0x00);
+                ReWriteByte(filename, 0xFF);
+                ReWriteRand(filename);
+            }
+            // Метод Шнайдера - 7 проходов
+            else if (method == 2)
+            {
+                ReWriteByte(filename, 0xFF);
+                ReWriteByte(filename, 0x00);
+                for (int i = 0; i < 5; i++) ReWriteRand(filename);
+            }
+
+            ReWriteEmpty(filename);
+        }
+
+        //переименовает случайным образом файл и удаляет его.
+        static void RenameAndDeleteFile(string filename)
+        {
+            string newfilename = filename.Substring(0, filename.LastIndexOf('\\') + 1) + GenRandomString(5);
+            System.IO.File.Move(filename, newfilename);
+            System.IO.File.Delete(newfilename);
+        }
+
+        //рекурсивно обходит папку и составляет список файлов и директорий
+        static List<string> getFileList(string DirName)
+        {
+            List<string> fileList = System.IO.Directory.GetFiles(DirName).ToList<string>();
+            foreach (string dir in System.IO.Directory.GetDirectories(DirName))
+            {
+                fileList.Add(dir);
+                fileList.AddRange(getFileList(dir));
+            }
+            return fileList;
+        }
+
+        static List<string> RemoveFiles(List<string> fileList, byte method) {
+            List<string> errors = new List<string>();
+
+            // Убиваем файлы
+            foreach (string file in fileList)
+            {
+                try
+                {
+                    if (System.IO.File.Exists(file))
+                    {
+                        WipeFile(file, method);
+                        RenameAndDeleteFile(file);
+                    }
+                }
+                catch
+                {
+                    errors.Add(file);
+                }
+            }
+            return errors;
+        }
+
+        static List<string> RemoveDirs(List<string> dirList)
+        {
+            List<string> errors = new List<string>();
+            foreach (string dir in dirList)
+            {
+                try
+                {
+                    if (System.IO.Directory.Exists(dir))
+                    {
+                        Console.WriteLine(dir);
+                        string newdirname = dir.Substring(0, dir.LastIndexOf('\\') + 1) + GenRandomString(5);
+                        System.IO.Directory.Move(dir, newdirname);
+                        System.IO.Directory.Delete(newdirname);
+                    }
+                }
+                catch
+                {
+                    errors.Add(dir);
+                }
+            }
+            return errors;
+        }
+        static void AddMenu()
+        {
+            try
+            {
+                RegistryKey RegHandle = Registry.ClassesRoot.OpenSubKey(@"\Directory\shell\", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                RegHandle = RegHandle.CreateSubKey("Wiper");
+                RegHandle.SetValue("", "Secure Delete");
+                RegHandle.SetValue("Icon", Application.ExecutablePath);
+                RegHandle = RegHandle.CreateSubKey("command");
+                RegHandle.SetValue("", Application.ExecutablePath + " \"%1\"");
+
+                /*RegHandle = Registry.ClassesRoot.OpenSubKey(@"\Drive\shell\", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                RegHandle = RegHandle.CreateSubKey("Wiper");
+                RegHandle.SetValue("", "Secure Delete");
+                RegHandle.SetValue("Icon", Application.ExecutablePath);
+                RegHandle = RegHandle.CreateSubKey("command");
+                RegHandle.SetValue("", Application.ExecutablePath + " \"%1\"");*/
+
+                RegHandle = Registry.ClassesRoot.OpenSubKey(@"\*\shell\", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                RegHandle = RegHandle.CreateSubKey("Wiper");
+                RegHandle.SetValue("", "Secure Delete");
+                RegHandle.SetValue("Icon", Application.ExecutablePath);
+                RegHandle = RegHandle.CreateSubKey("command");
+                RegHandle.SetValue("", Application.ExecutablePath + " \"%1\"");
+            }
+            catch { }
+        }
+
+        static void DelMenu()
+        {
+            try
+            {
+                RegistryKey RegHandle = Registry.ClassesRoot.OpenSubKey(@"\Directory\shell\", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                RegHandle.DeleteSubKeyTree("Wiper");
+                /*RegHandle = Registry.ClassesRoot.OpenSubKey(@"\Drive\shell\", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                RegHandle.DeleteSubKeyTree("Wiper");*/
+                RegHandle = Registry.ClassesRoot.OpenSubKey(@"\*\shell\", RegistryKeyPermissionCheck.ReadWriteSubTree);
+                RegHandle.DeleteSubKeyTree("Wiper");
+            }
+            catch { }
+        }
+
+        static int Main(string[] args)
+        {
+            byte delmethod = 2;
+            bool hidemode = false;
+
+            if (args.Length > 0)
+            {
+                List<String> arg = args.ToList<string>();
+                List<String> fileList = new List<string>();
+
+                foreach (string elem in args)
+                {
+                    if (String.Compare(elem, "-s") == 0)
+                    {
+                        Form1 form = new Form1();
+                        form.ShowDialog();
+                        return 0;
+                    }
+                    else if (String.Compare(elem, "-i") == 0)
+                    {
+                        AddMenu();
+                        return 0;
+                    }
+                    else if (String.Compare(elem, "-u") == 0)
+                    {
+                        DelMenu();
+                        return 0;
+                    }
+                    else if (elem.IndexOf("-m") == 0)
+                    {
+                        try
+                        {
+                            Console.WriteLine();
+                            delmethod = Convert.ToByte(args[0].Substring(2, 1));
+                            if (delmethod < 0 || delmethod > 2) delmethod = 2;
+                        }
+                        catch
+                        {
+                            delmethod = 2;
+                        }
+                    }
+                    else if (String.Compare(elem, "-h") == 0)
+                    {
+                        hidemode = true;
+                    }
+                    // Рекурсивно ищем директории.
+                    else if (System.IO.Directory.Exists(elem))
+                    {
+                        fileList.Add(elem);
+                        fileList.AddRange(getFileList(elem));
+                    }
+                    else if (System.IO.File.Exists(elem))
+                    {
+                        fileList.Add(elem);
+                    }
+                }
+
+                DialogResult res = DialogResult.No;
+                if (fileList.Count <= 1){ hidemode = true; }
+                if (!hidemode) res = MessageBox.Show("Delete " + fileList.Count.ToString() + " objects?", "Shredder", MessageBoxButtons.YesNo);
+                
+                if ((res == DialogResult.Yes) || (hidemode)) {
+
+                    List<string> errors = new List<string>();
+
+                    fileList.Reverse(); //костыль
+
+                    // Убиваем файлы
+                    errors.AddRange(RemoveFiles(fileList, delmethod));
+
+                    // Убиваем папки
+                    errors.AddRange(RemoveDirs(fileList));
+
+                    if (errors.Count > 0) { MessageBox.Show("Don't delete " + errors.Count.ToString() + "objects"); }
+                }
+            }
+            return 0;
+        }
+    }
+}
